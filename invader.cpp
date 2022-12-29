233128295b23337978e8620b96b86f27a50bb72e
July Rocha <nrochajuly@gmail.com>
2022-12-29 16:22:47 -0300

    :sparkles: Create sum and product variables

--- invader.cpp
+++ invader.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "You are a secret agent breaking into a secure server room \n";
-    std::cout << "You need to enter the correct codes to continue...";
+    cout << "You are a secret agent breaking into a secure server room \n";
+    cout << "You need to enter the correct codes to continue...\n";
+
+    int a = 4;
+    int b = 3;
+    int c = 2;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    cout << sum << endl;
+    cout << product;
+
     return 0;
 }
